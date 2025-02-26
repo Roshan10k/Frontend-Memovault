ac91b365c79adc446f80e5274717e57df1957f54
Roshan Khadka <khadkaasd963@gmail.com>
2025-02-26 21:21:54 +0545

    creating achievement page

---
+++ src/pages/AchievementsPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import AchievementsSection from "../component/AchievementSection";
+
+const AchievementsPage = () => {
+  return (
+    <div>
+      
+      <AchievementsSection />
+    </div>
+  );
+};
+
+export default AchievementsPage;
